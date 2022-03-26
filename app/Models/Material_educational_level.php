9d8d9ab957ec6d48be55051f479b007cb167d195
urbano4725 <efurbano@misena.edu.co>
2022-03-25 23:37:59 -0500

    first commit

---
+++ app/Models/Material_educational_level.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Material_educational_level extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('full_name'); 
+            $table->string('document_type');
+            $table->string('document_number');
+            $table->string('certificate_misak');
+            
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken(); 
+            
+            /* $table->unsignedBigInteger('rols_id');
+            $table->foreign('rols_id')->references('id')->on('rols')->onDelete('cascade')->onUpdate('cascade');*/
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
